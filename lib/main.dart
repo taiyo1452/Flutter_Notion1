325e62c0fac1e0cb00e80bbbe659a91e228acb46
taiyo1452 <hmtaiyo989@gmail.com>
2025-12-18 10:20:24 +0900

    add: textWidget

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,39 @@ class _FlutterLesson extends State<FlutterLesson> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(child: Column(mainAxisAlignment: .center)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+
+          children: <Widget>[
+            const Text("普通のテキスト"),
+
+            Text(
+              'TextStyleで装飾したテキスト',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+              ),
+            ),
+
+            Text(
+              "適当に影をつけたテキスト",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 24,
+                shadows: [
+                  Shadow(
+                    blurRadius: 3.0,
+                    color: Colors.grey,
+                    offset: Offset(2.0, 2.0),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
